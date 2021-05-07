87bdd934ac80b97d7c5067c1f91dbf533b8dfc25
Suhas darsi <darsisuhas666@gmail.com>
2021-05-07 07:11:01 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express()
+const bodyParser = require('body-parser');
+const request = require('request');
+const apiKey = 'a5c9bc4a360bb8acea76d439ebd9f45c';
+
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
+
+app.get('/', function (req, res) {
+  res.render('index', {weather: null, error: null});
+})
+
+app.post('/', function (req, res) {
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+  request(url, function (err, response, body) {
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body)
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
+
